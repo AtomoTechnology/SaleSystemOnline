6509eb70f7a41fa54833e3a79e05e0901114f32b
atomo <atomotechnology@gmail.com>
2021-07-09 23:03:25 -0300

    DB

--- src/DB/db.js
+++ src/DB/db.js
@@ -3,8 +3,8 @@ import mysql from 'mysql';
 const mysqlconnection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'',
-    database:'onlinedatabase',
+    password:'root',
+    database:'onlinesystem',
     insecureAuth : true
 });
 mysqlconnection.connect( (err) =>{
